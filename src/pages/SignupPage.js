8daee7af9a207605184aa0ab1f9a49eac1aff4e7
Aditya <adityabharti.aktu2020@gmail.com>
2024-02-06 15:52:57 +0530

    Login and SignUp

---
+++ src/pages/SignupPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Signup from "../features/Auth/Component/Signup";
+
+const SignupPage = () => {
+  return <div>
+    <Signup/>
+  </div>;
+};
+
+export default SignupPage;
